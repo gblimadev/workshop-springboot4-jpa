fdc0a6fe910fa326fa1b465060840c0aec3b3415
Gabriel Lima <gabrielosaulo@gmail.com>
2026-07-20 19:05:29 -0300

    OrderStatus enum, Correcao Classe Instant com formato ISO 8601

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,12 +1,17 @@
 package com.educandoweb.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
@@ -15,12 +20,21 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		User u1 = new User(null, "Maria Brown", "mara@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Paulo Green", "paulo@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.DELIVERED);
+		Order o2 = new Order(null, Instant.parse("2019-06-20T03:22:07Z"), u2, OrderStatus.CANCELED);
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2));
 	}
 } 
